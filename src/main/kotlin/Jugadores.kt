bdc2dcd908be0fb28ece6753aadf40ffd646c472
mario <mariodatos@outlook.com>
2021-11-02 05:14:54 +0100

    eje

---
+++ src/main/kotlin/Jugadores.kt
@@ -0,0 +1,4 @@
+class Jugadores {
+    var numero:Int=0
+    var puntos:Int=0
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,58 @@
+
+import kotlin.random.Random
+
 fun main() {
-    val dado1 = Dado()
-    val dado2 = Dado()
+    var lista = mutableListOf<Int>()
+    var dado1=dado()
 
-    dado1.darValores(3,4)
-    dado2.darValores(0,30)
+    var dado2=dado(1,3)
 
-    println("El dado1 ha obtenido un en tiradaUnica ${dado1.tiradaUnica()}")
-    println("El dado1 ha obtenido un en tiradaDoble ${dado1.tiradaDoble()}")
+    var dado3=dado(1,6)
 
-    println("El dado2 ha obtenido un en tiradaUnica ${dado2.tiradaUnica()}")
-    println("El dado2 ha obtenido un en tiradaDoble ${dado2.tiradaDoble()}")
+
+
+    repeat(100){
+
+        var sumatoria=0
+
+        var player=jugador()
+
+        repeat(100){
+            player.sumaResultados += (dado1.aletorio() + dado2.aletorio() + dado3.aletorio())
+
+        }
+        lista.add(player.sumaResultados)
+
+
+
+    }
+
+    println(lista)
+
+
+
 
 }
+class dado(min:Int=1,max:Int=6){
+    var min=min
+    var max=max
+    init {
+        darValores(min,max)
+    }
+    fun darValores(nMin:Int,nMax:Int){
+        min=nMin
+        max=nMax
+    }
+
+    fun aletorio():Int{
+        return Random.nextInt(min,max+1)
+    }
+
+}
+
+class jugador{
+
+    var sumaResultados:Int=0
+
+}
+
